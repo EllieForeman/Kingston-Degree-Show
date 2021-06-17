60a29e164a96d3f60f7272c8609ad491e09d4173
EllieForeman <foreman.eleanor@gmail.com>
2021-06-17 10:55:00 +0100

    gallery page now working

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,34 +1,55 @@
-// import React from "react"
-// import { useStaticQuery, graphql } from "gatsby"
-// import { GatsbyImage, getImage } from "gatsby-plugin-image"
-// import Layout from "../components/layout"
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import get from 'lodash/get'
+import "./gallery.css"
 
-// const Gallery = () => {
-//   const data = useStaticQuery(graphql`
-//     {
-//         allJson {
-//             edges {
-//               node {
-//                 value
-//               }
-//             }
-//           }
-//     }
-//   `)
-//   return (
-//     <Layout>
-//       <h1>All PDF Downloads</h1>
-//       <ul>
-//         {data.allFile.edges.map((file, index) => {
-//           console.log('file.node.publicURL', file.node.publicURL)
-//           return (
-//             <div>
-//             <GatsbyImage image="src/images/profile-photos/IMG_9523.jpeg"/>
-//             </div>
-//           )
-//         })}
-//       </ul>
-//     </Layout>
-//   )
-// }
-// export default Gallery
+class ProductPage extends React.Component {
+  render() {
+
+    const products = get(this, 'props.data.allDataJson.edges')
+    console.log(products)
+
+    return (
+      <Layout>
+        <div className="outerDiv">
+            {products.map(({ node }) => {
+                return (
+                    <div key={node.artist}>
+                    {/* <p>{node.artist}</p> */}
+                    <img class="profileImage"
+                    src={node.profileImage.childImageSharp.fluid.src}
+                    alt={node.alt}/>
+                    </div>
+                )
+            })}
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default ProductPage
+
+
+export const productsQuery = graphql`
+  query {
+    allDataJson {
+      edges {
+        node {
+          artist
+          email
+          link
+          profileImage {
+            childImageSharp {
+              fluid(maxWidth: 1000, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          alt
+        }
+      }
+    }
+  }
+`
