8838c9522c39b46eb124624a990b152baccbdf81
EllieForeman <foreman.eleanor@gmail.com>
2021-06-20 16:40:26 +0100

    installed background package but no support for gifs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,68 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery } from 'gatsby'
+import React from 'react'
+import styled from 'styled-components'
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import splashGif from '../images/profile-photos/testgif3.gif'
-import '../pages/index.css'
+import BackgroundImage from 'gatsby-background-image'
 
+const ArtDirectedBackground = ({ className }) => {
+  const { mobileImage, desktopImage } = useStaticQuery(
+    graphql`
+      query {
+        mobileImage: file(relativePath: { eq: "logo.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 490, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        desktopImage: file(relativePath: { eq: "testgif.gif" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+  // Set up the array of image data and `media` keys.
+  // You can have as many entries as you'd like.
+  const sources = [
+    mobileImage.childImageSharp.fluid,
+    {
+      ...desktopImage.childImageSharp.fluid,
+      media: `(min-width: 491px)`,
+    },
+  ]
 
-const IndexPage = () => (
-  <section className="splashLayout">
-    <Seo title="Home" />
-    <img className="splashGif" src={splashGif} alt="Otter dancing with a fish" />
+  return (
+    <BackgroundImage
+      Tag={`section`}
+      id={`media-test`}
+      className={className}
+      fluid={sources}
+    >
+      <StyledInnerWrapper>
+        <h2>Hello art-directed gatsby-background-image.</h2>
+      </StyledInnerWrapper>
+    </BackgroundImage>
+  )
+}
 
-      <Link className="enterButton" to="/gallery/">enter</Link> <br />
-  </section>
-)
+const StyledInnerWrapper = styled.div`
+  /* margin-top: 10%; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
-export default IndexPage
+const StyledArtDirectedBackground = styled(ArtDirectedBackground)`
+  width: 100%;
+  min-height: 100vh;
+  /* You should set a background-size as the default value is "cover"! */
+  background-size: cover;
+  /* So we won't have the default "lightgray" background-color. */
+  background-color: transparent;
+`
+
+export default StyledArtDirectedBackground
