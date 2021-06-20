3752e45b18177ed59756556a911cd57e9faef92b
EllieForeman <foreman.eleanor@gmail.com>
2021-06-20 15:32:51 +0100

    started on splash page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,19 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import splashGif from '../images/profile-photos/testgif3.gif'
+import '../pages/index.css'
+
 
 const IndexPage = () => (
-  <Layout>
+  <section className="splashLayout">
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/gallery/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+    <img className="splashGif" src={splashGif} alt="Otter dancing with a fish" />
+
+      <Link className="enterButton" to="/gallery/">enter</Link> <br />
+  </section>
 )
 
 export default IndexPage
