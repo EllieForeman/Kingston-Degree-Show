dc6717da0c3b16621a762f309768580a48a3e40a
EllieForeman <foreman.eleanor@gmail.com>
2021-06-22 13:51:45 +0100

    trial social icons

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -129,14 +129,10 @@ class ProductPage extends React.Component {
                                 }
                                 <div className="overlay">
                                     <div className="text">
-                                        <p>{node.artist} <FaInstagram /></p>
+                                        <p>{node.artist}</p>
                                         <p><a href="https://twitter.com/gatsbyjs" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></p>
-
-                                        <div className="socialIcons">
-                                            <p><a href="https://twitter.com/gatsbyjs" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></p>
-                                            <p><a href="https://twitter.com/gatsbyjs" target="_blank" rel="noopener noreferrer"><CgProfile /></a></p>
-                                            <p><a href="https://twitter.com/gatsbyjs" target="_blank" rel="noopener noreferrer"><HiOutlineMailOpen /></a></p>
-                                        </div>
+                                        <p><a href="https://twitter.com/gatsbyjs" target="_blank" rel="noopener noreferrer"><CgProfile /></a></p>
+                                        <p><a href="https://twitter.com/gatsbyjs" target="_blank" rel="noopener noreferrer"><HiOutlineMailOpen /></a></p>
                                     </div>
                                 </div>
                             </div>
